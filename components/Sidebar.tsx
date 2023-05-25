3612d862e3e58f8ba123ff77b3997d79389b6e56
rons9n <rons9n@gmail.com>
2023-05-25 15:20:52 -0500

    SidebarItem component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,8 +5,8 @@ import { BiSearch } from "react-icons/bi";
 import { twMerge } from "tailwind-merge";
 import { usePathname } from "next/navigation";
 
+import SidebarItem from "./SidebarItem";
 import Box from "./Box";
-
 import { useMemo } from "react";
 
 interface SidebarProps {
@@ -35,7 +35,7 @@ const Sidebar = ({ children }: SidebarProps) => {
   );
 
   return (
-    <div className={twMerge()}>
+    <div>
       <div
         className="
           hidden 
@@ -49,7 +49,11 @@ const Sidebar = ({ children }: SidebarProps) => {
         "
       >
         <Box>
-          <div className="flex flex-col gap-y-4 px-5 py-4"></div>
+          <div className="flex flex-col gap-y-4 px-5 py-4">
+            {routes.map((item) => (
+              <SidebarItem key={item.label} {...item} />
+            ))}
+          </div>
         </Box>
         <Box className="overflow-y-auto h-full">
           {/* <Library songs={songs} /> */}
