f080df48bda2cb10549f1c5801c76deb87eb819b
rons9n <rons9n@gmail.com>
2023-05-28 11:36:19 -0500

    Price interface

--- types.ts
+++ types.ts
@@ -18,3 +18,18 @@ export interface Product {
   image?: string;
   metadata?: Stripe.Metadata;
 }
+
+export interface Price {
+  id: string;
+  product_id?: string;
+  active?: boolean;
+  description?: string;
+  unit_amount?: number;
+  currency?: string;
+  type?: Stripe.Price.Type;
+  interval?: Stripe.Price.Recurring.Interval;
+  interval_count?: number;
+  trial_period_days?: number | null;
+  metadata?: Stripe.Metadata;
+  products?: Product;
+}
