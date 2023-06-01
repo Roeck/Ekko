c62900fb837ce6fbfaf4f905937721fc3d57d29d
rons9n <rons9n@gmail.com>
2023-06-01 17:01:09 -0500

    PlayButton component

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import { Song } from "@/types";
 import useLoadImage from "@/hooks/useLoadImage";
 
+import PlayButton from "./PlayButton";
+
 interface SongItemProps {
   data: Song;
   onClick: (id: string) => void;
@@ -70,7 +72,9 @@ const SongItem: React.FC<SongItemProps> = ({ data, onClick }) => {
           bottom-24 
           right-5
         "
-      ></div>
+      >
+        <PlayButton />
+      </div>
     </div>
   );
 };
