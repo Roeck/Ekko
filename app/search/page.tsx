deda4c6957e4ce3e185a6ecf4798da25ffb989bb
rons9n <rons9n@gmail.com>
2023-06-02 18:50:37 -0500

    SearchInput component

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,5 @@
 import getSongsByTitle from "@/actions/getSongsByTitle";
-// import SearchInput from "@/components/SearchInput";
+import SearchInput from "@/components/SearchInput";
 import Header from "@/components/Header";
 
 import SearchContent from "./components/SearchContent";
@@ -27,7 +27,7 @@ const Search = async ({ searchParams }: SearchProps) => {
       <Header className="from-bg-neutral-900">
         <div className="mb-2 flex flex-col gap-y-6">
           <h1 className="text-white text-3xl font-semibold">Search</h1>
-          {/* <SearchInput /> */}
+          <SearchInput />
         </div>
       </Header>
       <SearchContent songs={songs} />
