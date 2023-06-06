16ba75ec848e05ce16169d951e2323d9f306fc40
rons9n <rons9n@gmail.com>
2023-06-06 13:07:52 -0500

    When shut down, stop playing songs functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,17 +3,17 @@
 import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { useRouter } from "next/navigation";
-import { useSupabaseClient } from "@supabase/auth-helpers-react";
-
 import { FaUserAlt } from "react-icons/fa";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { toast } from "react-hot-toast";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
-import { toast } from "react-hot-toast";
-
-import Button from "./Button";
 
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
+import usePlayer from "@/hooks/usePlayer";
+
+import Button from "./Button";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -21,6 +21,7 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const player = usePlayer();
   const router = useRouter();
   const authModal = useAuthModal();
 
@@ -29,12 +30,11 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
 
   const handleLogout = async () => {
     const { error } = await supabaseClient.auth.signOut();
-    // TODO: Reset any playing songs
+    player.reset();
     router.refresh();
+
     if (error) {
       toast.error(error.message);
-    } else {
-      toast.success("Logged out");
     }
   };
 
@@ -53,6 +53,7 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
       <div className="w-full mb-4 flex items-center justify-between">
         <div className="hidden md:flex gap-x-2 items-center">
           <button
+            onClick={() => router.back()}
             className="
               rounded-full 
               bg-black 
@@ -67,6 +68,7 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             <RxCaretLeft className="text-white" size={35} />
           </button>
           <button
+            onClick={() => router.forward()}
             className="
               rounded-full 
               bg-black 
@@ -83,6 +85,7 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
         </div>
         <div className="flex md:hidden gap-x-2 items-center">
           <button
+            onClick={() => router.push("/")}
             className="
               rounded-full 
               p-2 
@@ -98,6 +101,7 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             <HiHome className="text-black" size={20} />
           </button>
           <button
+            onClick={() => router.push("/search")}
             className="
               rounded-full 
               p-2 
