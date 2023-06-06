afa8efa6761e8f3026ea7825502b5662368dfe4a
rons9n <rons9n@gmail.com>
2023-06-06 12:51:37 -0500

    getActiveProductWithPrices action with props passing

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -4,8 +4,13 @@ import { useEffect, useState } from "react";
 import AuthModal from "@/components/AuthModal";
 import UploadModal from "@/components/UploadModal";
 import SubscribeModal from "@/components/SubscribeModal";
+import { ProductWithPrice } from "@/types";
 
-const ModalProvider = () => {
+interface ModalProviderProps {
+  products: ProductWithPrice[];
+}
+
+const ModalProvider: React.FC<ModalProviderProps> = ({ products }) => {
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
@@ -20,7 +25,7 @@ const ModalProvider = () => {
     <>
       <AuthModal />
       <UploadModal />
-      {/* <SubscribeModal /> */}
+      <SubscribeModal products={products} />
     </>
   );
 };
